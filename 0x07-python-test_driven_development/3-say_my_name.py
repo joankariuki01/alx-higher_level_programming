8d89170d52bc0c0b4b6cdb49befc6d387d91cf22
joankariuki01 <contact.joankariuki@gmail.com>
2024-02-22 04:23:11 -0800

    Add solution to task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
+""""
 
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
+This module contains a function that prints a name
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    '''This function prints name (<first name> <last name>)
+
+    Args:
+        first_name (str): The fisrt name to be printed
+        last_name (str): The last name to be printed
+
+    Raises:
+        TypeError: If either the first_name and last_name are not strings
+
+    '''
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
