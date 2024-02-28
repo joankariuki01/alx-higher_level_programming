2d63307b4fef503ef65b8390387befeca1e1bcc5
joankariuki01 <contact.joankariuki@gmail.com>
2024-02-28 04:33:09 -0800

    Add new solution to task 8, 9, 10, 11, 12

--- test-script.py
+++ test-script.py
@@ -1,17 +1,2 @@
-class Base():
-    """ My base class """
-
-    __nb_instances = 0
-
-    def __init__(self):
-        Base.__nb_instances += 1
-        self.id = Base.__nb_instances
-
-class User(Base):
-    """ My User class """
-
-    def __init__(self):
-        self.id = 89
-
-u = User()
-print(u.id)
+a = {'id': 89, 'name': "John", 'projects': [1, 2, 3, 4]}
+a.get('projects')[3]
